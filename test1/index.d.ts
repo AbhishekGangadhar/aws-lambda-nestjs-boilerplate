884417b7171450519ce0d53b43434687eba55416
Inteli-Turus <inteliturus@gmail.com>
2021-08-09 20:49:57 +0300

    remove test1 directory

--- test1/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { INestApplication } from '@nestjs/common';
-import { Express } from 'express';
-import { Context } from 'aws-lambda';
-import { Response } from 'aws-serverless-express';
-export declare function createApp(expressApp: Express): Promise<INestApplication>;
-export declare function handler(event: any, context: Context): Promise<Response>;
